f6b61c42a46f1673aecbb2f846a9c0e99973d8f9
Richard Lyon <richthegeek@gmail.com>
2017-11-01 09:22:14 +0000

    Version 3.0.0

--- index.js
+++ index.js
@@ -1,1 +1,85 @@
-module.exports = require('./lib');
+const Beatrix = require('beatrix');
+const Sift = require('sift');
+const _ = require('lodash');
+
+class Manager {
+
+  connect (options, cb) {
+    this.name = options.name;
+    this.connection = Beatrix({
+      name: options.name,
+      uri: options.uri,
+      exchange: {
+        autoDelete: false,
+        durable: true,
+        type: 'topic'
+      },
+      responseQueue: false
+    });
+  }
+
+  filter (filter, message) {
+    if (_.isFunction(filter)) {
+      return filter(message);
+    }
+
+    if (_.isObject(filter)) {
+      return Sift(filter)(message)
+    }
+
+    return true
+  }
+
+  // runs the provided `method` for all messages
+  // matching the `event` passed
+  // supported options:
+  //  - name: queue name for logging, defaults to `event`
+  //  - type: queue name in rabbit, defaults to `event`
+  //  - routingKey: routing key in rabbit, defaults to `event`
+  //  - persistent: boolean, should the queue still exist in between app lifetimes? default: true
+  //  - autoDelete: boolean, should the queue delete when no consumers exist, based on `persistent`
+  //  - durable: boolean, should the queue survive a rabbitmq reboot, based on `persistent`
+  //  - filter: siftQuery or function that must return true for the event to be passed to the cb
+  on (event, options, method) {
+    options = _.defaults({}, options, {
+      name: this.name + '.' + event,
+      type: this.name + '.' + event,
+      routingKey: event,
+      autoDelete: options.persistent === false,
+      durable: options.persistent !== false,
+      context: this
+    });
+
+    options.process = this.process.bind(this, options, method)
+
+    this.connection.createQueue(event, options)
+  }
+
+  process (options, method, message) {
+    let body = _.defaults(message.body, {
+      type: message.fields.routingKey,
+      typeArray: message.fields.routingKey.split('.')
+    });
+
+    if (!this.filter(options.filter, message.body)) {
+      return message.resolve('Filtered');
+    }
+
+    method(message.body, (err, res) => {
+      if (err) {
+        message.retry(true);
+        message.reject(err);
+      } else {
+        message.resolve(res);
+      }
+    });
+  }
+
+  // publishes an event to the queue
+  emit (event, body) {
+    console.log('emit', event, body)
+    return this.connection.publish(event, body, {routingKey: event});
+  }
+}
+
+module.exports = new Manager()
